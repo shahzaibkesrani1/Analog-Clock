c63e3eb801ade53808eee7819a642ba6777233da
ShahzaibKesrani <111377043+shahzaibkesrani1@users.noreply.github.com>
2023-07-01 03:04:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// Phele sabko Get Karlenge 
+
+const body = document.querySelector("body"),
+  hourHand = document.querySelector(".hour"),
+  minuteHand = document.querySelector(".minute"),
+  secondHand = document.querySelector(".second"),
+  modeSwitch = document.querySelector(".mode-switch");
+
+//   Phir Local Stoge se get karrhe hai agar mode dark hai toh dark he rahe warna light hojjye
+if (localStorage.getItem("mode") === "Dark Mode") {
+  body.classList.add("dark");
+  modeSwitch.textContent = "Light Mode";
+}
+// Yeh mode swtich ke lie banaya hai toggle
+modeSwitch.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  const isDarkMode = body.classList.contains("dark");
+  modeSwitch.textContent = isDarkMode ? "Light Mode" : "Dark Mode";
+  localStorage.setItem("mode", isDarkMode ? "Dark Mode" : "Light Mode");
+});
+// Isme hum time update karva rahe kee second ki stick transform rotate hojye uske date.getSeconds ke hisab se 
+const updateTime = () => {
+  let date = new Date(),
+    secToDeg = (date.getSeconds() / 60) * 360,
+    minToDeg = (date.getMinutes() / 60) * 360,
+    hrToDeg = (date.getHours() / 12) * 360;
+  secondHand.style.transform = `rotate(${secToDeg}deg)`;
+  minuteHand.style.transform = `rotate(${minToDeg}deg)`;
+  hourHand.style.transform = `rotate(${hrToDeg}deg)`;
+};
+setInterval(updateTime, 1000);
+updateTime();
